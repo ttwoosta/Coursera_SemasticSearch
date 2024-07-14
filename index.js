ce42aebe1a73dd48c834898a5dc0433bb3df4ff7
Tu Tong <ttwoosta@gmail.com>
2024-07-14 07:11:01 -0400

    Organized module into its own files

--- index.js
+++ index.js
@@ -1,60 +1,21 @@
-import { openai, supabase } from './config.js';
+import { onSematicSearchBtnClick } from './sematic_search.js';
+import { onTextChunkingBtnClick } from './text_chunking.js';
 
-// User query about podcasts
-const query = "Something peaceful and relaxing";
-main(query);
+document.querySelector('#app').innerHTML = `
+  <div class="container">
+    <div class="d-flex justify-content-center" style="height: 50em">
+      <div class="align-self-center flex-column">
+        <div class="p-2"><button type="button" class="btn btn-primary" id="search-btn" >Semastic Search</button></div>
+        <div class="p-2"><button type="button" class="btn btn-primary" id="text-chunking-btn" >Text Chunking</button></div>
+      </div>
+    </div>
+  </div>
+`
 
-// Bring all function calls together
-async function main(input) {
-  const embedding = await createEmbedding(input);
-  const match = await findNearestMatch(embedding);
-  await getChatCompletion(match, input);
+document.getElementById('search-btn').onclick = (e) => {
+  onSematicSearchBtnClick();
+};
+
+document.getElementById('text-chunking-btn').onclick = (e) => {
+  onTextChunkingBtnClick(e);
 }
-
-// Create an embedding vector representing the input text
-async function createEmbedding(input) {
-  const embeddingResponse = await openai.embeddings.create({
-    model: "text-embedding-ada-002",
-    input
-  });
-  return embeddingResponse.data[0].embedding;
-}
-
-// Query Supabase and return a semantically matching text chunk
-async function findNearestMatch(embedding) {
-  const { data } = await supabase.rpc('match_documents', {
-    query_embedding: embedding,
-    match_threshold: 0.50,
-    match_count: 1
-  });
-  return data[0].content;
-}
-
-// Use OpenAI to make the response conversational
-const chatMessages = [{
-  role: 'system',
-  content: `You are an enthusiastic podcast expert who loves recommending 
-  podcasts to people. You will be given two pieces of information - 
-  some context about podcasts episodes and a question. 
-  Your main job is to formulate a short answer to the question 
-  using the provided context. If you are unsure and cannot find the answer 
-  in the context, say, "Sorry, I don't know the answer." 
-  Please do not make up the answer.` 
-}];
-
-async function getChatCompletion(text, query) {
-  chatMessages.push({
-    role: 'user',
-    content: `Context: ${text} Question: ${query}`
-  });
-  
-  const response = await openai.chat.completions.create({
-    model: 'gpt-4',
-    messages: chatMessages,
-    temperature: 0.5,
-    frequency_penalty: 0.5
-  });
-
-  console.log(response.choices[0].message.content);
-}
-
---
+++ text_chunking.js
@@ -0,0 +1,5 @@
+
+
+export function onTextChunkingBtnClick(e) {
+
+}
