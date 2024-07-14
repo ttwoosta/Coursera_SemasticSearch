a5a79a091ee506cd3494bc5f7b1d9f17f7e7e0eb
Tu Tong <ttwoosta@gmail.com>
2024-07-14 09:06:46 -0400

    Added text chunking func

--- text_chunking.js
+++ text_chunking.js
@@ -1,5 +1,15 @@
 
+import { CharacterTextSplitter } from "langchain/text_splitter";
 
-export function onTextChunkingBtnClick(e) {
+export async function onTextChunkingBtnClick(e) {
+  const response = await fetch('podcasts.txt');
+  const text = await response.text();
 
+  const splitter = new CharacterTextSplitter({
+    separator: " ",
+    chunkSize: 150,
+    chunkOverlap: 0,
+  });
+  const output = await splitter.createDocuments([text]);
+  console.log(output);
 }
